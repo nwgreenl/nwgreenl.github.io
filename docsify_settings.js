16c6d02b7518fb9b4cf574739584b92c8f6df18a
Nick Greenlees <greenleesnick@gmail.com>
2020-07-22 14:33:09 -0600

    Double Quotes

--- docsify_settings.js
+++ docsify_settings.js
@@ -1,17 +1,17 @@
 window.$docsify = {
-	name		: 'the-resting-dev',
-	repo		: 'nwgreenl/nwgreenl.github.io',
-	logo		: './media/me.png',
-	themeColor	: '#fff',
-	homepage	: 'home.md',
-	loadNavbar	: 'navbar.md',
-	loadSidebar	: 'sidebar.md',
-	notFoundPage	: '404.md',
-	auto2top		: true,
-	maxLevel		: 3,
-	subMaxLevel	: 3,
-	ga		: 'UA-158858778-3',
-	tabs		: {
+	name: "the-resting-dev",
+	repo: "nwgreenl/nwgreenl.github.io",
+	logo: "./media/me.png",
+	themeColor: "#fff",
+	homepage: "home.md",
+	loadNavbar: "navbar.md",
+	loadSidebar: "sidebar.md",
+	notFoundPage: "404.md",
+	auto2top: true,
+	maxLevel: 3,
+	subMaxLevel: 3,
+	ga:"UA-158858778-3",
+	tabs: {
 		persist	: false
 	},
 }
